acbc4bb48d00c9d9da85a2b72e964c5e5c1ec5c3
kwame-Owusu <alfredowusu595@gmail.com>
2025-12-04 19:42:16 +0000

    setup initial routing

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("making url shortener, accorcia is the name")
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Hey Accorcia, lets shorten some urls !",
+		})
+	})
+
+	err := r.Run(":9808")
+	if err != nil {
+		panic(fmt.Sprintf("Failed to start the web server - Error: %v", err))
+	}
 }
