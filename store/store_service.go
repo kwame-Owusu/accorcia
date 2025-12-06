cad21a35f42f614c41a4151404e58599ffa820dc
kwame-Owusu <alfredowusu595@gmail.com>
2025-12-06 20:57:23 +0000

    implement functions for saving mapping and retrieving from redis store

--- store/store_service.go
+++ store/store_service.go
@@ -46,6 +46,11 @@ We want to be able to save the mapping between the originalUrl
 and the generated shortUrl url
 */
 func SaveUrlMapping(shortUrl string, originalUrl string, userId string) {
+	err := storeService.redisClient.Set(ctx, shortUrl, originalUrl, CacheDuration).Err()
+	if err != nil {
+		panic(fmt.Sprintf("Failed saving key url | Error: %v - shorUrl: %s - OriginalUrl: %s", err, shortUrl, originalUrl))
+
+	}
 
 }
 
@@ -56,6 +61,9 @@ url, so what we need to do here is to retrieve the long url and
 think about redirect.
 */
 func RetrieveInitialUrl(shortUrl string) string {
-
-	return ""
+	result, err := storeService.redisClient.Get(ctx, shortUrl).Result()
+	if err != nil {
+		panic(fmt.Sprintf("Failed RetrieveInitialUrl url | Error: %v - shortUrl: %s", err, shortUrl))
+	}
+	return result
 }
