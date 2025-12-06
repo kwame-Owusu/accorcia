057974363911a99914b35dc15a0dcf47982b38d7
kwame-Owusu <alfredowusu595@gmail.com>
2025-12-06 20:51:03 +0000

    add store layer and its api

---
+++ store/store_service.go
@@ -0,0 +1,61 @@
+package store
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/go-redis/redis/v8"
+)
+
+// wrapper around raw redis client
+type StorageService struct {
+	redisClient *redis.Client
+}
+
+var (
+	storeService = &StorageService{}
+	ctx          = context.Background()
+)
+
+// Note that in a real world usage, the cache duration shouldn't have
+// an expiration time, an LRU policy config should be set where the
+// values that are retrieved less often are purged automatically from
+// the cache and stored back in RDBMS whenever the cache is full
+const CacheDuration = 6 * time.Hour
+
+func InitializeStore() *StorageService {
+	NewRedisClient := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
+	})
+
+	pong, err := NewRedisClient.Ping(ctx).Result()
+	if err != nil {
+		panic(fmt.Sprintf("Error initiating Redis: %v", err))
+	}
+
+	fmt.Printf("\nRedis started successfully: pong message: {%s}", pong)
+	storeService.redisClient = NewRedisClient
+	return storeService
+}
+
+/*
+We want to be able to save the mapping between the originalUrl
+and the generated shortUrl url
+*/
+func SaveUrlMapping(shortUrl string, originalUrl string, userId string) {
+
+}
+
+/*
+We should be able to retrieve the initial long URL once the short
+is provided. This is when users will be calling the shortlink in the
+url, so what we need to do here is to retrieve the long url and
+think about redirect.
+*/
+func RetrieveInitialUrl(shortUrl string) string {
+
+	return ""
+}
